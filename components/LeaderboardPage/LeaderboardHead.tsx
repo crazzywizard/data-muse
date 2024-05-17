618d41717f4b166c3bf05b1630d013b5f1936db7
SeniorDev <tech.eng.me@gmail.com>
2024-05-17 15:18:11 -0400

    I fix align

--- components/LeaderboardPage/LeaderboardHead.tsx
+++ components/LeaderboardPage/LeaderboardHead.tsx
@@ -7,20 +7,20 @@ const LeaderboardHead = () => (
   >
     <tr
       className="bg-[#BCB4C5] font-helvetica font-bold text-lg rounded rounded-lg"
-      tw="divide-y text-5xl divide-gray-200 dark:divide-gray-800 text-black flex justify-between w-full bg-[#BCB4C6] drop-shadow-2xl rounded rounded-lg"
+      tw="divide-y text-5xl divide-gray-200 dark:divide-gray-800 text-black flex w-full bg-[#BCB4C6] drop-shadow-2xl rounded rounded-lg"
       style={{
         boxShadow: '0 25px 50px rgba(0, 0, 0, 0.25)',
       }}
     >
       <th
-        className="px-4 py-3 text-left tracking-wider w-12"
-        tw="px-4 py-3 text-left font-medium tracking-wider w-12"
+        className="px-4 py-3 text-left tracking-wider"
+        tw="px-4 py-3 text-left font-medium tracking-wider w-[200px]"
       >
         rank
       </th>
       <th
         className="px-4 py-3 text-left tracking-wider"
-        tw="px-4 py-3 text-left font-medium tracking-wider"
+        tw="pl-[140px] py-3 text-left font-medium tracking-wider w-[565px]"
       >
         user
       </th>
