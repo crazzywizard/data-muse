ec9c4fe4fa3b0f2d8ab1facbdfcf2e85de90e246
sameer rasa <crazzywizard@gmail.com>
2024-08-12 20:14:27 -0400

    ability to convert ens name to eth address

--- app/creator/[creatorId]/page.tsx
+++ app/creator/[creatorId]/page.tsx
@@ -2,26 +2,35 @@ import { FRAME_INPUT_PLACEHOLDER, VERCEL_URL } from '@/lib/consts';
 import LeaderboardPage from '@/components/LeaderboardPage/LeaderboardPage';
 import { FrameMetadata } from '@coinbase/onchainkit';
 import getButtons from '@/lib/getButtons';
+import { isAddress } from 'viem';
+import { ethPublicClient } from '@/lib/publicClient';
 
-const Page = ({ params }: { params: { creatorId: string } }) => (
-  <div>
-    <FrameMetadata
-      ogTitle="data muse"
-      ogDescription="data muse"
-      buttons={getButtons()}
-      image={{
-        src: `${VERCEL_URL}/api/leaderboard?creator=${params.creatorId}`,
-        aspectRatio: '1:1',
-      }}
-      input={{
-        text: FRAME_INPUT_PLACEHOLDER,
-      }}
-      postUrl={`${VERCEL_URL}/api/frame`}
-    />
-    <meta property="of:accepts:xmtp" content="2024-02-01" />
+const Page = async ({ params }: { params: { creatorId: string } }) => {
+  const getProperAddress = async (creator: string) => {
+    return !isAddress(creator)
+      ? await ethPublicClient.getEnsAddress({ name: creator })
+      : params.creatorId;
+  };
+  return (
+    <div>
+      <FrameMetadata
+        ogTitle="data muse"
+        ogDescription="data muse"
+        buttons={getButtons()}
+        image={{
+          src: `${VERCEL_URL}/api/leaderboard?creator=${await getProperAddress(params.creatorId)}`,
+          aspectRatio: '1:1',
+        }}
+        input={{
+          text: FRAME_INPUT_PLACEHOLDER,
+        }}
+        postUrl={`${VERCEL_URL}/api/frame`}
+      />
+      <meta property="of:accepts:xmtp" content="2024-02-01" />
 
-    <LeaderboardPage creator={params.creatorId} />
-  </div>
-);
+      <LeaderboardPage creator={await getProperAddress(params.creatorId)} />
+    </div>
+  );
+};
 
 export default Page;
