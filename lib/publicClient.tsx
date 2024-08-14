6ccdd6e53ee53497f04cdac02e80b23dd25d0958
sameer rasa <crazzywizard@gmail.com>
2024-08-13 23:43:57 -0400

    adding alchemy to public client

--- lib/publicClient.tsx
+++ lib/publicClient.tsx
@@ -3,17 +3,17 @@ import { base, mainnet, optimism, arbitrum, zora } from 'viem/chains';
 
 export const ethPublicClient = createPublicClient({
   chain: mainnet,
-  transport: http('https://eth-mainnet.g.alchemy.com/v2/bID30XgZClKSEMoPxM1QGYk93jxlOHSs'),
+  transport: http(`https://eth-mainnet.g.alchemy.com/v2/${process.env.NEXT_PUBLIC_ALCHEMY_KEY}`),
 });
 
 export const optimismPublicClient = createPublicClient({
   chain: optimism,
-  transport: http('https://opt-mainnet.g.alchemy.com/v2/bID30XgZClKSEMoPxM1QGYk93jxlOHSs'),
+  transport: http(`https://opt-mainnet.g.alchemy.com/v2/${process.env.NEXT_PUBLIC_ALCHEMY_KEY}`),
 });
 
 export const basePublicClient = createPublicClient({
   chain: base,
-  transport: http('https://base-mainnet.g.alchemy.com/v2/bID30XgZClKSEMoPxM1QGYk93jxlOHSs'),
+  transport: http(`https://base-mainnet.g.alchemy.com/v2/${process.env.NEXT_PUBLIC_ALCHEMY_KEY}`),
 });
 
 export const arbitrumPublicClient = createPublicClient({
