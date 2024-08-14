8d63edd629febb38f9aeeaea70eb504021e084c3
sameer rasa <crazzywizard@gmail.com>
2024-08-13 23:38:49 -0400

    removing dev logging

--- hooks/useLeaderboard.tsx
+++ hooks/useLeaderboard.tsx
@@ -20,7 +20,6 @@ const useLeaderboard = (creator: string) => {
         ]);
         const zoraFiltered = getLeaderboard(zoraData.response, USD);
         const merged = mergeLeaderboardData(zoraFiltered, soundData);
-        console.log('mergedData: ', merged);
         const sorted = getSortedLeaderboard(merged);
         const named = await getNames(sorted.splice(0, 100));
         setLeaderboard(named);
